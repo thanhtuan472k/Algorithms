66602d9c135927d3963dd2b7fa1c9a783cdcf78d
Thanh Tuan <tuango.472000@gmail.com>
2021-08-30 23:51:30 +0700

    count unique value in an array

---
+++ Session-5-Problem-Solving/countUniqueValues.js
@@ -0,0 +1,17 @@
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  var i = 0;
+  for (var j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+let result = countUniqueValues([1, 2, 5, 9, 9.3, 100000]);
+let result2 = countUniqueValues([1, 0, -2, 323]);
+
+console.log(result);
+console.log(result2);
